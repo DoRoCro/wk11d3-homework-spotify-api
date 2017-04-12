4988ef98dcfaaa77bf855bfbe1445310d3b73609
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-04-12 16:34:08 +0100

    album listing retrieved

--- public/album_listing.js
+++ public/album_listing.js
@@ -8,5 +8,13 @@ AlbumListing.prototype = {
     var request = new XMLHttpRequest()
     request.open('GET', 'https://api.spotify.com/v1/search?q=' + this.search + '&type=album')
     console.log(request)
+    request.onload = function () {
+      if (request.status !== 200) return
+      var jsonString = request.responseText
+      var albums = JSON.parse(jsonString)
+      this.albums = albums
+      // this.onUpdate()
+    }.bind(this)
+    request.send()
   }
 }
